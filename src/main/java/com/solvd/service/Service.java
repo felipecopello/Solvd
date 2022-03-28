c47b45c6d165b81c09b1a91b8416c4e9866c6d93
felipe <felipecopello94@gmail.com>
2022-03-28 08:28:07 -0300

    update

--- src/main/java/com/solvd/service/Service.java
+++ src/main/java/com/solvd/service/Service.java
@@ -1,15 +1,12 @@
 package com.solvd.service;
 
 import com.solvd.Iaba.ClientQueue;
-import com.solvd.Iaba.SecurityGuard;
 
 public abstract class Service extends ClientQueue<Object> {
 	public static void main(String[] args) {
 
-		SecurityGuard securityGuard = new SecurityGuard();
 		// Librarian librarian1 = new Librarian();
 		// librarian1.recieveNewClient();
-		// recieveClientQueue(2);
-		securityGuard.listFurniture();
+		recieveClientQueue(2);
 	}
 }
