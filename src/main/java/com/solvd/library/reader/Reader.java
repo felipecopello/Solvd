a5ce1f66494882839bd21bd4a52da73223ba8be6
felipe <felipecopello94@gmail.com>
2022-04-04 18:53:49 -0300

    applied changes in reader

--- src/main/java/com/solvd/library/reader/Reader.java
+++ src/main/java/com/solvd/library/reader/Reader.java
@@ -3,26 +3,36 @@ package com.solvd.library.reader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Reader {
-	private static File file1 = new File(
-			"C:\\Users\\Felipe\\eclipse-workspace\\Library\\src\\main\\resource\\article.txt");
-	private static File outputFile = new File(
-			"C:\\Users\\Felipe\\eclipse-workspace\\Library\\src\\main\\java\\com\\solvd\\library\\reader\\output.txt");
-	private static ArrayList<String> arrayOutput = new ArrayList<>();
+	private static final Logger LOGGER = LogManager.getLogger(Reader.class);
+	private static final File INPUT_FILE = new File("src/main/resources/article.txt");
+	private static final File OUTPUT_FILE = new File("output.txt");
+	private static TreeSet<String> arrayOutput = new TreeSet<>();
 
-	public static void main(String[] arg) throws IOException {
-		String fileAsString = FileUtils.readFileToString(file1, StandardCharsets.UTF_8);
-		String[] fileAsStrArray = StringUtils.split(fileAsString);
+	private static Pattern pattern = Pattern.compile("\\s|,|\\.|\\(|\\)");;
 
-		for (int i = 0; i < fileAsStrArray.length; i++) {
-			int n = StringUtils.countMatches(fileAsString, fileAsStrArray[i]);
-			arrayOutput.add(fileAsStrArray[i] + "__" + n);
+	public static void main(String[] arg) {
+		try {
+			String fileAsString = FileUtils.readFileToString(INPUT_FILE, StandardCharsets.UTF_8);
+			String lowerFileString = fileAsString.toLowerCase();
+			String[] fileAsStrArray = pattern.split(lowerFileString);
+
+			for (int i = 0; i < fileAsStrArray.length; i++) {
+				int n = StringUtils.countMatches(lowerFileString, fileAsStrArray[i]);
+				arrayOutput.add(fileAsStrArray[i] + "__" + n);
+			}
+			FileUtils.writeLines(OUTPUT_FILE, arrayOutput);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
-		FileUtils.writeLines(outputFile, arrayOutput);
+		LOGGER.info("The program exited without error.");
 	}
 }
